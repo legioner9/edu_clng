4649d0ee7c462f86812e350a773169a03ea24768
[alt]legioner9 <legioner9@inbox.ru>
2026-05-01 22:04:51 +0700

    <>

--- .d/.osdn/prata/prt_002/001/platinum.c
+++ .d/.osdn/prata/prt_002/001/platinum.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
-int main(){
-    float
+// page book:: 080
+
+int main()
+{
+    float weght; //
+    float value; //
+
+    printf("Enter you weght (kg)");
+    scanf("%f", &weght);
+    value = weght * 1000;
+    printf("you weght in gramm: %.2f \n", value);
+    return 0;
+
 }
