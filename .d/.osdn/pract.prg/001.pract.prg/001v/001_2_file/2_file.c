1afd1f45d99eaeb536e627d2082c37f595647c98
[alt]legioner9 <legioner9@inbox.ru>
2026-05-15 18:34:57 +0700

    <>

---
+++ .d/.osdn/pract.prg/001.pract.prg/001v/001_2_file/2_file.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// {{hint}}
+
+/*
+types :: viod int long float double (long long) (long double)
+printf %[?] :: 
+
+*/
+
+// page book:: XXX
+
+int main(){
+    printf("Hello World!\n");
+    return 0;
+}
---
+++ .d/.osdn/pract.prg/001.pract.prg/001v/001_2_file/fn.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void fn(void){
+    printf("%s\n", "Hello World)");
+
+}
