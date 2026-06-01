c1b4dbf0752f7c291182cbdaab7eeac0a64f0a4d
legioner9 <legioner9@inbox.ru>
2026-06-02 00:45:48 +0700

    <>

--- .d/.osdn/book.edu/seacord/002_obj_fn_type/012_type_fn/_.d/type_fn.c
+++ .d/.osdn/book.edu/seacord/002_obj_fn_type/012_type_fn/_.d/type_fn.c
@@ -1,18 +1,34 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 // {{hint}}
 
 /*
 types :: viod int long float double (long long) (long double)
-printf %[?] :: 
+printf %[?] ::
 
 */
 
-// page book:: XXX
+// page book:: 52
 
-int main(void){
-    printf("Hello World!\n");
+int max(int a, int b);
+
+int main(void) {
+
+    int a = 4;
+    int b = 6;
+    printf("a = %d, b = %d, max = %d\n", a, b, max(a, b));
+
+    a = 7;
+    b = 2;
+    printf("a = %d, b = %d, max = %d\n", a, b, max(a, b));
+
+    a = 1;
+    b = 1;
+    printf("a = %d, b = %d, max = %d\n", a, b, max(a, b));
+
     return 0;
 }
+
+int max(int a, int b) { return a > b ? a : b; }
--- .d/.osdn/book.edu/seacord/002_obj_fn_type/012_type_fn/start.sh
+++ .d/.osdn/book.edu/seacord/002_obj_fn_type/012_type_fn/start.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-fn_7061_19902(){
+fn_19281_5437(){
 
-    local fl_pth_fn_7061_19902="${HOME}/edu_clng/.d/.osdn/book.edu/seacord/002_obj_fn_type/012_type_fn/start.sh"
-    local dr_pth_fn_7061_19902=$(dirname ${HOME}/edu_clng/.d/.osdn/book.edu/seacord/002_obj_fn_type/012_type_fn/start.sh)
-    local fl_nm_fn_7061_19902=$(basename ${HOME}/edu_clng/.d/.osdn/book.edu/seacord/002_obj_fn_type/012_type_fn/start.sh)
-    local rnd=7061_19902
+    local fl_pth_fn_19281_5437="${HOME}/edu_clng/.d/.osdn/book.edu/seacord/002_obj_fn_type/012_type_fn/start.sh"
+    local dr_pth_fn_19281_5437=$(dirname ${HOME}/edu_clng/.d/.osdn/book.edu/seacord/002_obj_fn_type/012_type_fn/start.sh)
+    local fl_nm_fn_19281_5437=$(basename ${HOME}/edu_clng/.d/.osdn/book.edu/seacord/002_obj_fn_type/012_type_fn/start.sh)
+    local rnd=19281_5437
 
     # [[ -z "$1" ]] && {
     #     # snp "out_err"
-    #     echo -e "${ECHO_RET1}in file://$fn_nm_fn_7061_19902 , line=${LINENO} :: ARG_1_NOT_DEFINE is ..., return 1${NRM}" >&2
+    #     echo -e "${ECHO_RET1}in file://$fn_nm_fn_19281_5437 , line=${LINENO} :: ARG_1_NOT_DEFINE is ..., return 1${NRM}" >&2
     #     return 1
     # }
 
     [[ "$1" == "-h" ]] && {
         echo -e "
-        this -h for fl_pth_fn_7061_19902 ::
+        this -h for fl_pth_fn_19281_5437 ::
         doing :: 
             exa use ::
             far use ::
@@ -23,18 +23,18 @@ fn_7061_19902(){
         return 0
     }
 
-    local arg_1_fn_7061_19902="$1"
-    local arg_2_fn_7061_19902="$2"
-    local arg_3_fn_7061_19902="$3"
+    local arg_1_fn_19281_5437="$1"
+    local arg_2_fn_19281_5437="$2"
+    local arg_3_fn_19281_5437="$3"
 
 	# [[ -z "$2" ]] && {
 	# 	# snp "out_err"
-	# 	echo -e "${ECHO_RET1}in file://$fn_nm_fn_7061_19902 , line=${LINENO} :: ARG_2_NOT_DEFINE is ..., return 1${NRM}" >&2
+	# 	echo -e "${ECHO_RET1}in file://$fn_nm_fn_19281_5437 , line=${LINENO} :: ARG_2_NOT_DEFINE is ..., return 1${NRM}" >&2
 	# 	return 1
 	# }
 
 	#! ptr_path
-	# local ptr_path_1="$arg_1_fn_7061_19902"
+	# local ptr_path_1="$arg_1_fn_19281_5437"
 	# ptr_path_1="$(l_01_abs_path "$(pwd)" "ptr_path_1")"
  
     # l_02_fs2f ins.f {{pre_str_in_rcv.f}} rcv.f
@@ -42,9 +42,9 @@ fn_7061_19902(){
     # lfoe_path_to_var ::  insert \${HOME} into string '${HOME}'
     # lfoe_gig_sh :: create $2 file in $1 dir from ${HOME}/aer_foe/.d/.sh/.lib.sh/.dta/.tml/sh1.tml
 
-    # cd ${dr_pth_fn_7061_19902} || return 1
-    # local dr_1_fn_7061_19902=${dr_pth_fn_7061_19902}/${arg_1_fn_7061_19902}
-    # local fl_1_fn_7061_19902=${dr_pth_fn_7061_19902}/${arg_1_fn_7061_19902}
+    # cd ${dr_pth_fn_19281_5437} || return 1
+    # local dr_1_fn_19281_5437=${dr_pth_fn_19281_5437}/${arg_1_fn_19281_5437}
+    # local fl_1_fn_19281_5437=${dr_pth_fn_19281_5437}/${arg_1_fn_19281_5437}
     # contane {\fn_nm\} {{body}}
     eval "cd \${dr_pth_fn_${rnd}}/_.d || return 1"
 
@@ -58,6 +58,6 @@ fn_7061_19902(){
 
 }
 
-fn_7061_19902 $@
-#DATE :: Пн 01 июн 2026 18:43:32 +07
+fn_19281_5437 $@
+#DATE :: Вт 02 июн 2026 00:36:54 +07
 #SGN :: . ${HOME}/edu_clng/.d/.osdn/book.edu/seacord/002_obj_fn_type/gig_dr.sh 012_type_fn ${HOME}/edu_clng/.d/.osdn/book.edu/seacord/002_obj_fn_type/_.f.d/_.f.001 type_fn.c 
