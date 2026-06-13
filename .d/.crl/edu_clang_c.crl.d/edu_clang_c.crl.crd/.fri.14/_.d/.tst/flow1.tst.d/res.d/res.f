535f1b53fae98b695ba7c6f0fba75a2949ae9ea0
legioner9 <legioner9@inbox.ru>
2026-06-14 02:35:21 +0700

    <>

---
+++ .d/.crl/edu_clang_c.crl.d/edu_clang_c.crl.crd/.fri.14/_.d/.tst/flow1.tst.d/res.d/res.f
@@ -0,0 +1,7 @@
+***start main()***
+a)-+
+b+(
+from a_aer_foe_c
+from a_edu_clang_c
+main
+***end main()***
---
+++ .d/.crl/edu_clang_c.crl.d/edu_clang_c.crl.crd/.fri.14/gig_dr.arg.sh
@@ -0,0 +1,3 @@
+. ~/edu_clng/.d/.crl/edu_clang_c.crl.d/edu_clang_c.crl.crd/.fri.14/gig_dr.arg.sh \
+	~/edu_clng/.d/.crl/edu_clang_c.crl.d/edu_clang_c.crl.crd/c11s_edu_clang_c.fn \
+	c11s_edu_clang_c.c
