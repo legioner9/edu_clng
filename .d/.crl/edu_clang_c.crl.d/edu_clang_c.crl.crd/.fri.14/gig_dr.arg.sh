25f44fe74c466838a6b4a3744160df2f426fd1be
legioner9 <legioner9@inbox.ru>
2026-06-14 02:45:59 +0700

    <>

--- .d/.crl/edu_clang_c.crl.d/edu_clang_c.crl.crd/.fri.14/gig_dr.arg.sh
+++ .d/.crl/edu_clang_c.crl.d/edu_clang_c.crl.crd/.fri.14/gig_dr.arg.sh
@@ -1,3 +1,3 @@
 . ~/edu_clng/.d/.crl/edu_clang_c.crl.d/edu_clang_c.crl.crd/.fri.14/gig_dr.arg.sh \
-	~/edu_clng/.d/.crl/edu_clang_c.crl.d/edu_clang_c.crl.crd/c11s_edu_clang_c.fn \
+	~/edu_clng/.d/.crl/edu_clang_c.crl.d/edu_clang_c.crl.crd/c11s_edu_clang_c.fn/001 \
 	c11s_edu_clang_c.c
